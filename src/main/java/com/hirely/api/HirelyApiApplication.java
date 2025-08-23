e336914843c375b48e78d5acae762e8c9613a9dc
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-08-23 16:40:40 -0300

    :tada: first commit - Starting a new project

---
+++ src/main/java/com/hirely/api/HirelyApiApplication.java
@@ -0,0 +1,13 @@
+package com.hirely.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirelyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirelyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hirely/api/HirelyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirely.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirelyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
